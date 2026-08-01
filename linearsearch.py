4a7461e6d32f38a80bb09cc067006bf42eff3cc7
manjunath111362 <reddymanjunath53095@gmail.com>
2026-08-01 22:40:04 +0530

    Delete linearsearch.py

--- linearsearch.py
+++
@@ -1,8 +0,0 @@
-def linear_search(arr, target):
-    for index, value in enumerate(arr):
-        if value == target:
-            return index
-    return -1 
-    
-numbers = [12, 45, 7, 23, 89, 34]
-print(linear_search(numbers, 23))
